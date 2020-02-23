ffe57aa78fa0e8b53280d13b9df7144732e928d5
Bradley <Bradbailey972@gmail.com>
2020-02-23 23:42:37 +0200

    styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '/registration', component: RegistrationComponent }
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'test', component: TestComponent}
 ];
 
 @NgModule({
